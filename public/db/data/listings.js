eef42a276a6669b5ec17880edf23f16127bfd028
Harry Markley <tremarkley@gmail.com>
2018-02-22 11:59:26 -0800

    Adjusted from 200 to 100 listings

--- public/db/data/listings.js
+++ public/db/data/listings.js
@@ -4,7 +4,7 @@ function randomNumberGenerator(minVal, maxVal) {
   return Math.floor(Math.random() * (maxVal - minVal)) + minVal;
 }
 
-for (let i = 1; i <= 200; i += 1) {
+for (let i = 1; i <= 100; i += 1) {
   const newListing = {};
   newListing.id = i;
   newListing.minimum_stay = randomNumberGenerator(1, 4);
