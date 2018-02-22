fc13cb3374297badea1e0fbe6c5e67d8b358edcd
Harry Markley <tremarkley@gmail.com>
2018-02-22 12:12:05 -0800

    Created class for mocking reservation data

--- public/db/data/reservations.js
+++ public/db/data/reservations.js
@@ -1,1 +1,22 @@
+const dates = require('./dates');
+const listings = require('./listings');
 
+const reservations = [];
+
+function randomNumberGenerator(minVal, maxVal) {
+  return Math.floor(Math.random() * (maxVal - minVal)) + minVal;
+}
+
+for (let i = 0; i < dates.length; i += 1) {
+  const dateId = i + 1;
+  for (let j = 0; j < listings.length; j += 1) {
+    const reservationObj = {};
+    reservationObj.listing_id = listings[j].id;
+    reservationObj.date_id = dateId;
+    reservationObj.price = randomNumberGenerator(100, 800);
+    reservationObj.available = Math.random() < 0.85;
+    reservations.push(reservationObj);
+  }
+}
+
+module.exports = reservations;
