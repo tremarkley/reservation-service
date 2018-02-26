ec41837e1ab06db1374c6e1ef4d0f8017774fabe
Harry Markley <tremarkley@gmail.com>
2018-02-26 14:28:46 -0800

    Add beforeEach to test so that database is re-seeded before each test

--- server/spec/db.test.js
+++ server/spec/db.test.js
@@ -1,20 +1,27 @@
 process.env.NODE_ENV = 'test';
 
 const reservations = require('../../db/controllers/reservations');
+const knex = require('../../db/knex');
 
-describe('getReservationDataForMonth returns reservation object', () => {
-  it('Should return object containing all reservations inside the reservations db', () =>
-    reservations.getReservationDataForMonth(1, 1, 2018)
-      .then((reservationsArray) => {
-        for (let i = 0; i < reservationsArray.length; i += 1) {
-          expect(reservationsArray[i].listing_id).toBe(1);
-          expect(reservationsArray[i].month).toBe(1);
-          expect(reservationsArray[i].day).toBe(i + 1);
-          expect(reservationsArray[i].year).toBe(2018);
-          expect(reservationsArray[i]).toHaveProperty('minimum_stay');
-          expect(reservationsArray[i]).toHaveProperty('maximum_guests');
-          expect(reservationsArray[i]).toHaveProperty('price');
-          expect(reservationsArray[i]).toHaveProperty('available');
-        }
-      }));
+describe('Database Controller Methods', () => {
+  beforeEach(done => knex.seed.run()
+    .then(() => done()));
+
+  describe('getReservationDataForMonth returns reservation object', () => {
+    it('Should return object containing all reservations inside the reservations db', () =>
+      reservations.getReservationDataForMonth(1, 1, 2018)
+        .then((reservationsArray) => {
+          for (let i = 0; i < reservationsArray.length; i += 1) {
+            expect(reservationsArray[i].listing_id).toBe(1);
+            expect(reservationsArray[i].month).toBe(1);
+            expect(reservationsArray[i].day).toBe(i + 1);
+            expect(reservationsArray[i].year).toBe(2018);
+            expect(reservationsArray[i]).toHaveProperty('minimum_stay');
+            expect(reservationsArray[i]).toHaveProperty('maximum_guests');
+            expect(reservationsArray[i]).toHaveProperty('price');
+            expect(reservationsArray[i]).toHaveProperty('available');
+          }
+        }));
+  });
 });
+
