af54205d015c6f3f4feb6db4cfe6af8f59c24d4c
Harry Markley <tremarkley@gmail.com>
2018-02-27 14:48:58 -0800

    Styled footer with button

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+/*  eslint-disable no-unused-vars  */
 import css from '../styles/styles.css';
+/*  eslint-enable no-unused-vars  */
 
 const App = () => (
-  <div className="reservations-footer"><p>test</p></div>
+  <div className="reservations-footer">
+    <div className="content">
+      <button className="bookButton">Book</button>
+    </div>
+  </div>
 );
 
 export default App;
