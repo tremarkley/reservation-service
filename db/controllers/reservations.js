f1a0808476b9d93fd6eba4f4cd85b55de90da2d0
Harry Markley <tremarkley@gmail.com>
2018-02-26 17:49:22 -0800

    Implement makeReservation with all tests passing

--- db/controllers/reservations.js
+++ db/controllers/reservations.js
@@ -1,23 +1,5 @@
 const knex = require('../../db/knex.js');
 
-//  get reservations for each day of the month for specified month, year, and listing
-// Array of Reservation Objects for each day of the month
-// Fields:
-// Listing_Id: Number
-// Minimum-stay: Number
-// Maximum-guests: Number
-// Month: Number
-// Day: Number
-// Year: Number
-// Price: Number
-// Available: Boolean
-
-// 0. get the listing information from the listing table
-// 1. get all date id's for the given month
-// 2. for each date id look for it's corresponding reservation in the reservation table
-// 3. put all of this information into a single object
-
-// right now we are getting {"listing_id":1,"date_id":1,"price":"300.00","available":true}
 // gets all reservation data for the month on a specified listingId, month and year
 // ordered by day in ascending order
 const getReservationDataForMonth =
@@ -39,18 +21,9 @@ function getReservationDataForMonth(listingId, month, year) {
   });
 };
 
-//  input: listing id, check in date, check out date (mm-dd-yyyy)
-//  When getting date id's do a query by >= check in date and < check out date
-//  first thing we need to do is check if the reservation is even possible
-//  loop through all dates from check in to check out (dont include check out)
-//  if available is true for each then we must go through and change each to false and call resolve
-//  if available is false for one then we must call reject
-//  output is total price of reservation
 const makeReservation = function makeReservation(listingId, checkInDate, checkOutDate) {
   let price = 0;
   return new Promise((resolve, reject) => {
-    // const checkInDateObj = new Date(checkInDate);
-    // const checkOutDateObj = new Date(checkOutDate);
     knex('dates').where('date', '>=', checkInDate).andWhere('date', '<', checkOutDate).select('id')
       .then((dateIdObjects) => {
         const dateIds = [];
@@ -62,7 +35,7 @@ const makeReservation = function makeReservation(listingId, checkInDate, checkOu
       .then((reservations) => {
         const reservationIds = [];
         for (let i = 0; i < reservations.length; i += 1) {
-          price += reservations[i].price;
+          price += +reservations[i].price;
           reservationIds.push(reservations[i].id);
           if (!reservations[i].available) {
             throw new Error('Reservation not available through dates!');
--- server/spec/db.test.js
+++ server/spec/db.test.js
@@ -8,8 +8,9 @@ describe('Database Controller Methods', () => {
     .then(() => done()));
 
   describe('getReservationDataForMonth', () => {
-    it('Should return object containing all reservations inside the reservations db', () =>
-      reservations.getReservationDataForMonth(1, 1, 2018)
+    it('Should return object containing all reservations inside the reservations db', () => {
+      expect.hasAssertions();
+      return reservations.getReservationDataForMonth(1, 1, 2018)
         .then((reservationsArray) => {
           for (let i = 0; i < reservationsArray.length; i += 1) {
             expect(reservationsArray[i].listing_id).toBe(1);
@@ -21,15 +22,37 @@ describe('Database Controller Methods', () => {
             expect(reservationsArray[i]).toHaveProperty('price');
             expect(reservationsArray[i]).toHaveProperty('available');
           }
-        }));
+        });
+    });
   });
 
   describe('makeReservation', () => {
     it('Should throw an error if dates are not all available', () => {
-      expect.hasAssertions();
-      return reservations.makeReservation(1, '1-1-2018', '1-4-2018')
-        .catch((err) => {
-          expect(err.message).toEqual('Reservation not available through dates!');
+      expect.assertions(1);
+      return expect(reservations.makeReservation(1, '1-1-2018', '1-4-2018')).rejects.toEqual(new Error('Reservation not available through dates!'));
+    });
+    it('Should throw an error if dates are not all available', (done) => {
+      expect.assertions(1);
+      reservations.makeReservation(1, '1-1-2018', '1-4-2018')
+        .catch(() => {
+          reservations.getReservationDataForMonth(1, 1, 2018)
+            .then((reservationsArray) => {
+              expect(reservationsArray[0].available).toBe(true);
+              done();
+            });
+        });
+    });
+    it('Should change availability to false for all days between check in and check out if a reservation is made successfully', (done) => {
+      expect.assertions(3);
+      reservations.makeReservation(1, '1-1-2018', '1-3-2018')
+        .then(price => expect(price).toBe(600))
+        .then(() => {
+          reservations.getReservationDataForMonth(1, 1, 2018)
+            .then((reservationsArray) => {
+              expect(reservationsArray[0].available).toBe(false);
+              expect(reservationsArray[1].available).toBe(false);
+              done();
+            });
         });
     });
   });
