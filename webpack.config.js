78fa41b6a1714f17677d93bde249baea2a9ffe33
Harry Markley <tremarkley@gmail.com>
2018-02-27 13:22:16 -0800

    Configure webpack and react

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'client/dist');
+const APP_DIR = path.resolve(__dirname, 'client/app');
+
+const config = {
+  entry: `${APP_DIR}/index.jsx`,
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
+
+module.exports = config;
