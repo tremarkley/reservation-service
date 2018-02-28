d6a0fd8b6bf378d8b3462c71fe2c73032e3891bd
Harry Markley <tremarkley@gmail.com>
2018-02-27 17:28:56 -0800

    Part way through styling dialog. Got two boxes showing up an aligned for guest number and reservation dates

--- client/app/components/popup.jsx
+++ client/app/components/popup.jsx
@@ -8,7 +8,48 @@ const Popup = () => (
           <div className="close-dialog-container">
             <button className="close-button" />
           </div>
-          <div className="reservations-dialog-container" />
+          <div className="reservations-dialog-container">
+            <div className="dates">
+              <label htmlFor="dates-container" className="reservation-label">
+                <span>Dates</span>
+              </label>
+              <div id="dates-container" className="dates-container">
+                <div className="checkin-container">
+                  <input
+                    type="text"
+                    id="checkin"
+                    name="checkin"
+                    value=""
+                    placeholder="Check In"
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="hyphen">-</div>
+                <div className="checkout-container">
+                  <input
+                    type="text"
+                    id="checkout"
+                    name="checkout"
+                    value=""
+                    placeholder="Check Out"
+                    autoComplete="off"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="guests">
+              <label htmlFor="guests-button" className="reservation-button">
+                <span>Guests</span>
+              </label>
+              <button id="guests-button" className="guests-button">
+                <div className="guests-container">
+                  <span className="guest-text">
+                    <span>1 guest</span>
+                  </span>
+                </div>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
