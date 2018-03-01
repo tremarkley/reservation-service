2804161ceb1fcb55f2859b36c7dafe3858a1b698
Harry Markley <tremarkley@gmail.com>
2018-02-28 18:37:14 -0800

    Setup header and arrows on calendar

--- client/app/components/calendar.jsx
+++ client/app/components/calendar.jsx
@@ -6,17 +6,22 @@ import css from '../../styles/styles.css';
 const Calendar = () => (
   <div className="outer-calendar-pop-up">
     <div className="inner-calendar-pop-up">
-      <div className="calendar-holder"></div>
-
-    </div>
-    <div className="calendar-footer">
-      <div className="calendar-footer-content">
-        <small className="fine-print">
-          <div>Minimum Stay Varies</div>
-          <div>Updated Today</div>
-        </small>
-        <div className="clear-dates-div">
-          <button className="clear-dates-btn">Clear dates</button>
+      <div className="calendar-holder">
+        <div className="calendarHeader">
+          <button className="left-arrow" />
+          <h4 className="calendar-header">January 2018</h4>
+          <button className="right-arrow" />
+        </div>
+      </div>
+      <div className="calendar-footer">
+        <div className="calendar-footer-content">
+          <small className="fine-print">
+            <div>Minimum Stay Varies</div>
+            <div>Updated Today</div>
+          </small>
+          <div className="clear-dates-div">
+            <button className="clear-dates-btn">Clear dates</button>
+          </div>
         </div>
       </div>
     </div>
