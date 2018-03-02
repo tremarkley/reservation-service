4808a231b2d8fc569f166312bf7f98a14290dad8
Harry Markley <tremarkley@gmail.com>
2018-03-02 11:46:40 -0800

    Configure calendar to highlight dates between check in and check out

--- client/app/components/calendarGrid.jsx
+++ client/app/components/calendarGrid.jsx
@@ -5,6 +5,24 @@ import PropTypes from 'prop-types';
 import css from '../../styles/styles.css';
 /*  eslint-enable no-unused-vars  */
 
+const getActualDate = function getActualDate(day) {
+  return new Date(`${day.month}-${day.day}-${day.year}`);
+};
+
+const getDateClass = function getDateClass(day, dates) {
+  if (dates.checkInDate === undefined && dates.checkOutDate === undefined) {
+    return '';
+  } else if (dates.checkInDate === day || dates.checkOutDate === day) {
+    return 'date-selected';
+  } else if (dates.checkInDate !== undefined && dates.checkOutDate !== undefined) {
+    if (getActualDate(day) > getActualDate(dates.checkInDate)
+      && getActualDate(day) < getActualDate(dates.checkOutDate)) {
+      return 'between-selected';
+    }
+  }
+  return '';
+};
+
 const addRows = function addRows(days, clickHandler, dates) {
   let cells = [];
   const results = [];
@@ -23,7 +41,9 @@ const addRows = function addRows(days, clickHandler, dates) {
       }
     }
 
-    cells.push(<td key={`date-${(results.length + 1) * cells.length}`} className={`date ${dates.checkInDate === days[i] || dates.checkOutDate === days[i] ? 'date-selected' : null}`} onClick={() => clickHandler(days[i])}>{days[i].day}</td>);
+    // if date is between check in and check out then give it class between
+
+    cells.push(<td key={`date-${(results.length + 1) * cells.length}`} className={`date ${getDateClass(days[i], dates)}`} onClick={() => clickHandler(days[i])}>{days[i].day}</td>);
 
     //  last day of the month
     if (i === days.length - 1) {
