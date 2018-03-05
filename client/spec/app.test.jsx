60e41829e4a786b837a7f4051b3518d8f8384a69
Harry Markley <tremarkley@gmail.com>
2018-03-05 11:47:19 -0800

    Refactor test to create a shallow instance and update reservationData before each test

--- client/spec/app.test.jsx
+++ client/spec/app.test.jsx
@@ -8,32 +8,39 @@ describe('<App /> component', () => {
   beforeAll(() => {
     mockdate.set('1/1/2018');
   });
+  let wrapper;
+  let instance;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+    instance = wrapper.instance();
+    instance.updateReservationData(reservationData.array, 0, 2018);
+  });
 
   it('Clicking book button should render popup component', () => {
-    const wrapper = shallow(<App />);
+    // const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
     wrapper.find('button').simulate('click');
     expect(wrapper).toMatchSnapshot();
   });
   //  these tests must be here because the functions for opening the calendar are at the app level
   it('Clicking checkin button should render calendar component', () => {
-    const wrapper = mount(<App />);
+    wrapper = mount(<App />);
     wrapper.find('button').simulate('click');
     wrapper.find('#checkin').simulate('click');
     expect(wrapper).toMatchSnapshot();
   });
 
   it('Clicking outside of checkin/checkout button should close calendar component', () => {
-    const wrapper = mount(<App />);
+    wrapper = mount(<App />);
     wrapper.find('button').simulate('click');
     wrapper.find('.pop-up').simulate('click');
     expect(wrapper).toMatchSnapshot();
   });
 
   it('Should calculate lastPossibleCheckOutDate when a checkin is selected', () => {
-    const wrapper = shallow(<App />);
-    const instance = wrapper.instance();
-    instance.updateReservationData(reservationData.array, 0, 2018);
+    // const wrapper = shallow(<App />);
+    // const instance = wrapper.instance();
+    // instance.updateReservationData(reservationData.array, 0, 2018);
     const lastPossibleCheckOut = instance.findLastPossibleCheckOutDate({
       listing_id: 1,
       minimum_stay: 3,
@@ -57,9 +64,9 @@ describe('<App /> component', () => {
   });
 
   it('Should calculate lastPossibleCheckInDate when a checkout is selected', () => {
-    const wrapper = shallow(<App />);
-    const instance = wrapper.instance();
-    instance.updateReservationData(reservationData.array, 0, 2018);
+    // const wrapper = shallow(<App />);
+    // const instance = wrapper.instance();
+    // instance.updateReservationData(reservationData.array, 0, 2018);
     const lastPossibleCheckIn = instance.findLastPossibleCheckInDate({
       listing_id: 1,
       minimum_stay: 3,
@@ -78,6 +85,40 @@ describe('<App /> component', () => {
       day: 7,
       year: 2018,
       price: '237.00',
+      available: true,
+    });
+  });
+
+  it('Should set lastPossibleCheckInDate as the current check in date if there is one', () => {
+    instance.handleCheckInClick();
+    instance.handleDateClick({
+      listing_id: 1,
+      minimum_stay: 3,
+      maximum_guests: 2,
+      month: 1,
+      day: 5,
+      year: 2018,
+      price: '634.00',
+      available: true,
+    });
+    const lastPossibleCheckIn = instance.findLastPossibleCheckInDate({
+      listing_id: 1,
+      minimum_stay: 3,
+      maximum_guests: 2,
+      month: 1,
+      day: 6,
+      year: 2018,
+      price: '284.00',
+      available: false,
+    });
+    expect(lastPossibleCheckIn).toEqual({
+      listing_id: 1,
+      minimum_stay: 3,
+      maximum_guests: 2,
+      month: 1,
+      day: 5,
+      year: 2018,
+      price: '634.00',
       available: true,
     });
   });
