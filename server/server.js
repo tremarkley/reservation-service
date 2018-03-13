ad66829a468e63c696f5624c127da82bb59c35e5
Harry Markley <tremarkley@gmail.com>
2018-03-12 19:06:10 -0700

    Modified port for server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-const port = process.env.port || 3002;
+const port = process.env.port || 3008;
 
 app.listen(port, () => console.log(`Reservation service listening on port ${port}!`));
