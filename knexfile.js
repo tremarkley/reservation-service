01ccb91fbc563141fa95a827cc3fe96959584be1
Harry Markley <tremarkley@gmail.com>
2018-03-12 19:22:01 -0700

    Updated snapshots, update background-image url to use single quotes, update test knexfile to point to localhost

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   test: {
     client: 'pg',
-    connection: 'postgres://postgres@database:5432/reservations_test',
+    connection: 'postgres://localhost:5432/reservations_test',
     seeds: {
       directory: path.join(__dirname, '/db/seeds/test'),
     },
